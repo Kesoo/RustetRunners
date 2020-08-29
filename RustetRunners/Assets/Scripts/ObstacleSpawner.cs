044d51bbbe5733aff0961ad69181f032872900da
Anton Ericson <antonericson97@gmail.com>
2020-08-29 16:06:51 +0200

    Added an obstacle spawner to spawn obstacles

---
+++ RustetRunners/Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+
+    public GameObject obstacle;
+
+    private float timeLeftUntilSpawn;
+    public float timeBetweenSpawn;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeLeftUntilSpawn <= 0)
+        {
+            Instantiate(obstacle, transform.position, Quaternion.identity);
+            timeLeftUntilSpawn = timeBetweenSpawn;
+        } else
+        {
+            timeLeftUntilSpawn -= Time.deltaTime;
+        }
+    }
+}
