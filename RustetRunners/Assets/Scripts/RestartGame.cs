842258641fb1d0f1e1bc5757fe3a04f231ff2e74
Anton Ericson <antonericson97@gmail.com>
2020-08-29 21:08:56 +0200

    Added game over screen and restart functionality

---
+++ RustetRunners/Assets/Scripts/RestartGame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartGame : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("RESTARTSCRIPT ACTIVE");
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
