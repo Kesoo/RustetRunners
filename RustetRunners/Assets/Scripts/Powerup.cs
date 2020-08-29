5dbedb63b1a935a7165daa5df5cf9d5227472be0
Marcus <marmathi@student.chalmers.se>
2020-08-29 22:52:25 +0200

    Added sound effects to points and powerups

--- RustetRunners/Assets/Scripts/Powerup.cs
+++ RustetRunners/Assets/Scripts/Powerup.cs
@@ -6,6 +6,7 @@ public class Powerup : MonoBehaviour
 {
     public int score;
     public int speed;
+    public GameObject pickup;
 
     // Update is called once per frame
     void Update()
@@ -15,6 +16,7 @@ public class Powerup : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collider){
         if(collider.CompareTag("Player")){
+            Instantiate(pickup, transform.position, Quaternion.identity);
             //collider.GetComponent<PlayerStats>().score += this.score;
             Destroy(gameObject);
         }
