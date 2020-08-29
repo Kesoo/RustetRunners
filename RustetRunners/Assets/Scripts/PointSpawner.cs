ab6cfc2e2c784ba57d5479effd330cab6141a00f
Marcus <marmathi@student.chalmers.se>
2020-08-29 20:55:31 +0200

    Points will now spawn at varying heights

---
+++ RustetRunners/Assets/Scripts/PointSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointSpawner : MonoBehaviour
+{
+
+    public GameObject obstacle;
+
+    private float timeLeftUntilSpawn;
+    public float maxTimeBetweenSpawn;
+    private const int maxHeight = 6;
+    private const int minHeight = 3;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeLeftUntilSpawn <= 0)
+        {
+            Instantiate(obstacle, new Vector2(11.59f, Random.Range(minHeight, maxHeight)), Quaternion.identity);
+            timeLeftUntilSpawn = Random.Range(1,maxTimeBetweenSpawn);
+        } else
+        {
+            timeLeftUntilSpawn -= Time.deltaTime;
+        }
+    }
+}
