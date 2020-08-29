34dbb19a3e9d48420014aec7db40794d4d10c3c7
Marcus <marmathi@student.chalmers.se>
2020-08-29 15:51:11 +0200

    Added a score counter, incrementing score every other second from game start

--- RustetRunners/Assets/Scripts/PlayerStats.cs
+++ RustetRunners/Assets/Scripts/PlayerStats.cs
@@ -1,21 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStats : MonoBehaviour
 {
     public int health;
-    private int score;
+    public int score;
+    private bool incremented;
 
     // Start is called before the first frame update
     void Start()
     {
         this.score = 0;
+        this.incremented = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if((int)Time.time % 2 == 0 && !incremented){
+            this.score += 1;
+            incremented = true;
+        } else if((int)Time.time % 2 != 0 && incremented){
+            incremented = false;
+        }
     }
 }
